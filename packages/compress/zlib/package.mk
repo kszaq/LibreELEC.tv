af2b2e89cbf468e93918961a8dd322e947367ed8
kszaq <kszaquitto@gmail.com>
2019-05-23 23:57:43 +0200

    zlib: update to zlib-ng

--- packages/compress/zlib/package.mk
+++ packages/compress/zlib/package.mk
@@ -2,12 +2,15 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="zlib"
-PKG_VERSION="1.2.11"
-PKG_SHA256="4ff941449631ace0d4d203e3483be9dbc9da454084111f97ea0a2114e19bf066"
+PKG_VERSION="4230228b86594f9e3561e444421c4df95f3638c7"
+PKG_SHA256="14f9430514d99a6b6857accd439abb3425a230beab89fed85cc378bf0a19e533"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.zlib.net"
-PKG_URL="http://zlib.net/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_URL="https://github.com/zlib-ng/zlib-ng/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_HOST="cmake:host"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="A general purpose (ZIP) data compression library."
 PKG_TOOLCHAIN="cmake-make"
+
+PKG_CMAKE_OPTS_HOST="-DZLIB_COMPAT=ON"
+PKG_CMAKE_OPTS_TARGET="-DZLIB_COMPAT=ON"
