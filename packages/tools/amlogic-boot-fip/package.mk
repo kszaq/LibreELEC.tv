9ef24277c393d6752d284f709382e4b1d85da258
Christian Hewitt <christianshewitt@gmail.com>
2022-01-27 01:18:11 +0000

    amlogic-boot-fip: bump to test odroid-c1 fake-fip

--- packages/tools/amlogic-boot-fip/package.mk
+++ packages/tools/amlogic-boot-fip/package.mk
@@ -3,8 +3,8 @@
 
 PKG_NAME="amlogic-boot-fip"
 PKG_LICENSE="nonfree"
-PKG_VERSION="7ff0004e0e4d261ba81334a2f46302bd06704aca"
-PKG_SHA256="bd8012388cca6a01e819c3fcce2522ca6a32f7287ac2b901efbbe90978dfe74d"
+PKG_VERSION="a69907ca1bd8a6c7490ae8cd6b72447ce8bd8d0f"
+PKG_SHA256=""
 PKG_SITE="https://github.com/LibreELEC/amlogic-boot-fip"
 PKG_URL="https://github.com/LibreELEC/amlogic-boot-fip/archive/${PKG_VERSION}.tar.gz"
 PKG_LONGDESC="Firmware Image Package (FIP) sources used to sign Amlogic u-boot binaries in LibreELEC images"
